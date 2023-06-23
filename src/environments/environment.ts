e0dba80f8ea0838e33e0e0e99f07647a0a2c0a24
Arthur Arakelyan <artur.araqelyan.2004@gmail.com>
2023-06-24 00:32:03 +0400

    fix(): [Gitignore] deleted environemt from .gitignore

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  prod: true,
+  firebase: {
+    apiKey: "AIzaSyCgLZPt0fj-TOrvJSuhVhPsPToJLd7LIq8",
+    authDomain: "keep-link-ae2c0.firebaseapp.com",
+    databaseURL: "https://keep-link-ae2c0.firebaseio.com",
+    projectId: "keep-link-ae2c0",
+    storageBucket: "keep-link-ae2c0.appspot.com",
+    messagingSenderId: "464197977425",
+    appId: "1:464197977425:web:4e2d52c9df5f27e1d0bf70",
+  },
+};
