36f7c3f70a942dcfc9b7e2124aa4f6eef13f7970
Arthur Arakelyan <artur.araqelyan.2004@gmail.com>
2023-07-08 00:34:21 +0400

    feat(44): [App] fixed async layout injection

--- src/app/store/hydration/hydartion.reducer.ts
+++ src/app/store/hydration/hydartion.reducer.ts
@@ -3,25 +3,28 @@ import { ActionReducer, INIT, UPDATE } from '@ngrx/store';
 // Store
 import { AppStore } from '../app.reducer';
 
+// Constants
+import { localStorageStoreKey } from '../../core/constants/local-storage-keys';
+
 export const hydrationMetaReducer = (
   reducer: ActionReducer<AppStore>,
 ): ActionReducer<AppStore> => {
   return (state, action) => {
     if (action.type === INIT || action.type === UPDATE) {
-      const storageValue = localStorage.getItem('state');
+      const storageValue = localStorage.getItem(localStorageStoreKey);
 
       if (storageValue) {
         try {
           return JSON.parse(storageValue);
         } catch {
-          localStorage.removeItem('state');
+          localStorage.removeItem(localStorageStoreKey);
         }
       }
     }
 
     const nextState = reducer(state, action);
 
-    localStorage.setItem('state', JSON.stringify(nextState));
+    localStorage.setItem(localStorageStoreKey, JSON.stringify(nextState));
 
     return nextState;
   };
