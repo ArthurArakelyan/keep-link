b27966c7d8d488dea070a41261505dd558080c03
Arthur Arakelyan <artur.araqelyan.2004@gmail.com>
2023-10-08 18:08:18 +0400

    feat(79): [Storybook] added fab storybook

---
+++ src/app/shared/components/fab/fab.stories.ts
@@ -0,0 +1,78 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+import { componentWrapperDecorator, moduleMetadata } from '@storybook/angular';
+
+// Modules
+import { SharedModule } from '../../shared.module';
+
+// Components
+import { FabComponent } from './fab.component';
+
+const meta: Meta<FabComponent> = {
+  title: 'Shared/FAB',
+  component: FabComponent,
+  tags: ['autodocs'],
+  render: (args: FabComponent) => ({
+    props: args,
+  }),
+  decorators: [
+    moduleMetadata({
+      imports: [SharedModule],
+    }),
+    componentWrapperDecorator(
+      (story) => `<div style="width: 100%; min-height: 100px;">${story}</div>`,
+    ),
+  ],
+  argTypes: {
+    icon: {
+      type: 'string',
+      defaultValue: 'plus',
+    },
+    label: {
+      type: 'string',
+      defaultValue: '',
+      description: 'aria-label',
+    },
+    disabled: {
+      type: 'boolean',
+      defaultValue: false,
+    },
+    loading: {
+      type: 'boolean',
+      defaultValue: false,
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<FabComponent>;
+
+export const FAB: Story = {
+  args: {
+    show: true,
+    icon: 'plus',
+    label: 'Add',
+    disabled: false,
+    loading: false,
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    show: true,
+    icon: 'plus',
+    label: 'Add',
+    disabled: true,
+    loading: false,
+  },
+};
+
+export const Loader: Story = {
+  args: {
+    show: true,
+    icon: 'plus',
+    label: 'Add',
+    disabled: false,
+    loading: true,
+  },
+};
